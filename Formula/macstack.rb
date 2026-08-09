8ab6742a16838bf6fc47c6100484ec652d97467a
Sebastian Fichtner <sebastian@nohype.ai>
2026-08-09 07:00:01 +0200

    Bump macstack to v0.2.12

--- Formula/macstack.rb
+++ Formula/macstack.rb
@@ -1,8 +1,8 @@
 class Macstack < Formula
   desc "Tech stack management based on a personal stack definition"
   homepage "https://macstack.dev"
-  url "https://github.com/nohype-ai/MacStack/archive/refs/tags/v0.2.11.tar.gz"
-  sha256 "f69a5dc9ca61ef84d39e2f96e1569e37192effd9a51fb8848499c4d146820246"
+  url "https://github.com/nohype-ai/MacStack/archive/refs/tags/v0.2.12.tar.gz"
+  sha256 "7d25b2d82894deb468dcc08847f663f9c02081ba8a9c4ea1a8093b3de66f70f4"
   license "MIT"
 
   depends_on "jq"
